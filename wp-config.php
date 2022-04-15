3fe5b8c308cd3e73f0586c89aa03e8593698e758
Lucio Moura <61831152+luciom20@users.noreply.github.com>
2022-04-15 10:40:51 -0300

    inicio
    inicio tailpress + nextjs

---
+++ wp-config.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * As configurações básicas do WordPress
+ *
+ * O script de criação wp-config.php usa esse arquivo durante a instalação.
+ * Você não precisa usar o site, você pode copiar este arquivo
+ * para "wp-config.php" e preencher os valores.
+ *
+ * Este arquivo contém as seguintes configurações:
+ *
+ * * Configurações do MySQL
+ * * Chaves secretas
+ * * Prefixo do banco de dados
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
+/** O nome do banco de dados do WordPress */
+define( 'DB_NAME', 'tailpress' );
+
+/** Usuário do banco de dados MySQL */
+define( 'DB_USER', 'root' );
+
+/** Senha do banco de dados MySQL */
+define( 'DB_PASSWORD', 'root' );
+
+/** Nome do host do MySQL */
+define( 'DB_HOST', 'localhost' );
+
+/** Charset do banco de dados a ser usado na criação das tabelas. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** O tipo de Collate do banco de dados. Não altere isso se tiver dúvidas. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Chaves únicas de autenticação e salts.
+ *
+ * Altere cada chave para um frase única!
+ * Você pode gerá-las
+ * usando o {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org
+ * secret-key service}
+ * Você pode alterá-las a qualquer momento para invalidar quaisquer
+ * cookies existentes. Isto irá forçar todos os
+ * usuários a fazerem login novamente.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '=w<8>]7[rsNAf56IIy#G,iGF?_2lULKH;sGlh}Hu@phbBa=>%}2]_?QqN,o0h~y4' );
+define( 'SECURE_AUTH_KEY',  'zYgL8TAD!]@^zxrzsN#bpk%/mH6S)wRfL8veAHLF`V)c52Gw( .>,ZHy:jkhFl >' );
+define( 'LOGGED_IN_KEY',    '|nzN:Mu%4g});sz~_bMd_>j@F97Uw$.Qs6+Z!fXhz;1+=$!w+}`u&RZ,wq~V4w~M' );
+define( 'NONCE_KEY',        '^n $@hCeFAt^s,F$fpkAcW(+EFP8CHgc0oCU@nf#W~s=WO&(lkI$a/U@Ib6B2~dv' );
+define( 'AUTH_SALT',        'Pzsap7Eh4hP,*-,SBlOW3G8E$`kfjy;~oN)O[mwqIz6gC|,2)1yAVqCV4@D`pGVo' );
+define( 'SECURE_AUTH_SALT', 'Cqt`PEce`:LhIrsDVT46[<RlmV4Bn9L3vaTg&YwY}p:e@WO&+E]tD,Sfo/1^Z5Hn' );
+define( 'LOGGED_IN_SALT',   'sb@V]V}6F!At!/XesrH@y_[s>{e/5L.K1?UJ=+eYzF[q-<ztJU6$c|Bg{ZdSJzb{' );
+define( 'NONCE_SALT',       'a  pdFOU&(Bs}8,[@_;$(bor -.l6H?e>dnx<?Ps3v>l3V?~_d&eI(d/&5`3Mz2x' );
+
+/**#@-*/
+
+/**
+ * Prefixo da tabela do banco de dados do WordPress.
+ *
+ * Você pode ter várias instalações em um único banco de dados se você der
+ * um prefixo único para cada um. Somente números, letras e sublinhados!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * Para desenvolvedores: Modo de debug do WordPress.
+ *
+ * Altere isto para true para ativar a exibição de avisos
+ * durante o desenvolvimento. É altamente recomendável que os
+ * desenvolvedores de plugins e temas usem o WP_DEBUG
+ * em seus ambientes de desenvolvimento.
+ *
+ * Para informações sobre outras constantes que podem ser utilizadas
+ * para depuração, visite o Codex.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Adicione valores personalizados entre esta linha até "Isto é tudo". */
+
+
+
+/* Isto é tudo, pode parar de editar! :) */
+
+/** Caminho absoluto para o diretório WordPress. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Configura as variáveis e arquivos do WordPress. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wp-content/themes/tailpress/functions.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * tailwind functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package tailwind
+ */
+
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+if ( ! function_exists( 'tailwind_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function tailwind_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on tailwind, use a find and replace
+		 * to change 'tailwind' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'tailwind', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus(
+			array(
+				'menu-1' => esc_html__( 'Primary', 'tailwind' ),
+			)
+		);
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+			)
+		);
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'tailwind_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 250,
+				'width'       => 250,
+				'flex-width'  => true,
+				'flex-height' => true,
+			)
+		);
+	}
+endif;
+add_action( 'after_setup_theme', 'tailwind_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function tailwind_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'tailwind_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'tailwind_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function tailwind_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'tailwind' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'tailwind' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'tailwind_widgets_init' );
+
+/**
+ * Get mix compiled asset.
+ *
+ * @param string $path The path to the asset.
+ *
+ * @return string
+ */
+function tailpress_get_mix_compiled_asset_url( $path ) {
+	$path                = '/' . $path;
+	$stylesheet_dir_uri  = get_stylesheet_directory_uri();
+	$stylesheet_dir_path = get_stylesheet_directory();
+
+	if ( ! file_exists( $stylesheet_dir_path . '/mix-manifest.json' ) ) {
+		return $stylesheet_dir_uri . $path;
+	}
+
+	$mix_file_path = file_get_contents( $stylesheet_dir_path . '/mix-manifest.json' );
+	$manifest      = json_decode( $mix_file_path, true );
+	$asset_path    = ! empty( $manifest[ $path ] ) ? $manifest[ $path ] : $path;
+
+	return $stylesheet_dir_uri . $asset_path;
+}
+
+/**
+ * Enqueue scripts and styles.
+ */
+function tailwind_scripts() {
+	wp_enqueue_style( 'tailwind-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_style_add_data( 'tailwind-style', 'rtl', 'replace' );
+	
+	wp_enqueue_style( 'tailpress', tailpress_get_mix_compiled_asset_url( 'css/app.css' ), array(), _S_VERSION );
+	
+	wp_enqueue_script( 'tailpress', tailpress_get_mix_compiled_asset_url( 'js/app.js' ), array( 'jquery' ), _S_VERSION );
+
+	wp_enqueue_script( 'tailwind-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	// Adding Alpine.js support
+    if(defined('ALPINEJS')){
+        
+        wp_enqueue_script( 'alpinejs', '//unpkg.com/alpinejs', array(), _S_VERSION );
+    }
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'tailwind_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
